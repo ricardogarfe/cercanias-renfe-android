05fbde6440caddbb7e4bf8b539419e43e09af581
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-05-02 17:53:22 +0200

    GPLv3 License

--- android/Renfe/src/com/jonsegador/Renfe/HorariosActivity.java
+++ android/Renfe/src/com/jonsegador/Renfe/HorariosActivity.java
@@ -1,10 +1,27 @@
-/*
- * Este fichero es parte de la aplicación Cercanías Renfe para Android
+/*  
+ * Copyright [2013] [Ricardo García Fernández] [ricarodgarfe@gmail.com]  
+ *  
+ * This file is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * Jon Segador <jonseg@gmail.com>
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * Podrás encontrar información sobre la licencia en el archivo LICENSE
- * https://github.com/jonseg/cercanias-renfe-android
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ * This file incorporates work covered by the following copyright and  
+ * permission notice: 
+ * 
+ *      Este fichero es parte de la aplicación Cercanías Renfe para Android
+ * 
+ *      Jon Segador <jonseg@gmail.com>
+ * 
+ *      Podrás encontrar información sobre la licencia en el archivo LICENSE
+ *      https://github.com/jonseg/cercanias-renfe-android
  */
 
 package com.jonsegador.Renfe;
